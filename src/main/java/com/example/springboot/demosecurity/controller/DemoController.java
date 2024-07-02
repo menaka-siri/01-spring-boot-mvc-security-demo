a98adcc8fd0b0809d4e57ee62ca714cf3a401160
Menaka S <menaka442@gmail.com>
2024-07-02 23:19:04 +0530

    restrict urls based on user roles

--- src/main/java/com/example/springboot/demosecurity/controller/DemoController.java
+++ src/main/java/com/example/springboot/demosecurity/controller/DemoController.java
@@ -6,7 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
     @GetMapping("/")
-    public String showHome(){
+    public String showHome() {
         return "home";
     }
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
 }
