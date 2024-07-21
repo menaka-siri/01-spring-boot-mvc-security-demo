f9d9a0728916359d4f3013b1723814f1e2c9e748
Menaka S <menaka442@gmail.com>
2024-07-21 20:33:08 +0530

    add mysql and jpa pom dependencies and datasource configs

--- src/main/java/com/example/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/example/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,14 +3,16 @@ package com.example.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -34,6 +36,8 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
 
+     */
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
@@ -55,6 +59,11 @@ public class DemoSecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
 }
